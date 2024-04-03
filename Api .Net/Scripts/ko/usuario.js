b962810035271781750e7526109bfb763b88cf94
LabInfo <LabInfo@TERREOA3M06>
2024-04-02 22:35:46 -0300

    Página "Api.cshtml" atualizada

--- Api .Net/Scripts/ko/usuario.js
+++ Api .Net/Scripts/ko/usuario.js
@@ -3,9 +3,22 @@
 var UsuarioModel = new function () {
     model = this;
     model.usuarios = ko.observable
+    model.email = ko.observable();
+    model.senha = ko.observable();
+    model.confirmarsenha = ko.observable();
+    model.comfirmar = ko.computed(function () {
+        return model.senha() == model.confirmarsenha();
+    }, this);
+    model.confirmarCor = ko.computed(function () { return model.comfirmar( ?'' : 'red'; }, this);
     model.carregar = function () {
         $.ajax("http://localhost:56458/api/Usuarios").done(data => model.usuarios(data));
     }
     model.carregar();
 }
+var host = document.location.protocol + "//" + document.location.host + ":" + document.location.port + "/api/Usurios"
+var UsuarioModel = new function () {
+    var nodel = this;
+   
+}
+
 ko.applyBindings(UsuarioModel);
